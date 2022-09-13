7db4208062edf7fc4a01cd567ac0ffcb8b50eec4
leonardofguedes <leonardo.fguedes@gmail.com>
2022-09-13 11:50:24 -0300

    Commit

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import user_login
-
+from django.contrib.auth import views as auth_views
+from socialmedia.views.dashboard import dashboard
 
 urlpatterns = [
-    path('login/', user_login, name='login'),
+    #path('login/', user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('dashboard/', dashboard, name='dashboard'),
     ]
